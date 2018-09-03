9218aab5cf819d23b49f5e2d218b495816851030
KYLE JARMAN <100568977@student.swin.edu.au>
2018-09-03 10:27:08 +1000

    Add project files.

---
+++ Week6Task/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Week6Task.Startup))]
+namespace Week6Task
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
